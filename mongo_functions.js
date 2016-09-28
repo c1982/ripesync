3a2bd109633abb5da80d0559605906aa82016c1e
c1982 <aspsrc@gmail.com>
2016-09-28 03:46:41 +0300

    Optimize distinct query

--- mongo_functions.js
+++ mongo_functions.js
@@ -1,15 +1,28 @@
 
 GetActiveIpCountByAsn
 function(asnumber) {
-    return db.getCollection('hostactivity').distinct("ip",{asn:asnumber}).length
+    return db.hostactivity.aggregate([
+    {$match: {asn:asnumber}},
+    {$group: {_id: "$ip"}},
+    {$group: { _id: 1, count: { $sum: 1}}}
+    ]);
 }
 
 GetIPCountByPortNumber
 function(asnumber, portnumber) {
-    return db.getCollection('hostactivity').distinct("ip",{asn:asnumber, ports: {$elemMatch: {port: portnumber}}}).length
+    
+return db.hostactivity.aggregate([
+    {$match: {asn:asnumber, ports: {$elemMatch: {port: portnumber}}}},
+    {$group: {_id: "$ip"}},
+    {$group: { _id: 1, count: { $sum: 1}}}
+    ]);
 }
 
 GetIPCountByTTLRange
 function(asnumber, low, hight) {
- return db.getCollection('hostactivity').distinct("ip",{asn:asnumber, ports: {$elemMatch: {ttl:{ $gte: low, $lt: hight }}}}).length
+    return db.hostactivity.aggregate([
+    {$match: {asn:asnumber, ports: {$elemMatch: {ttl:{ $gte: low, $lt: hight }}}}},
+    {$group: {_id: "$ip"}},
+    {$group: { _id: 1, count: { $sum: 1}}}
+    ]);
 }
